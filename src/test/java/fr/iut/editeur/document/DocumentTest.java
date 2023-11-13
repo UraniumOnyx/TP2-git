110d8b125b57af8a8c47ab53ff3df5b83019516f
Mickaël Coulon <coulonm@K114-1-25.dep-info.iutmontp.univ-montp2.fr>
2023-11-13 12:12:42 +0100

    tests

---
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -0,0 +1,27 @@
+package fr.iut.editeur.document;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class DocumentTest extends TestCase {
+
+    public void testAjouterTexte() {
+        Document banane = new Document();
+        banane.ajouter("Bonjour");
+        assertEquals(banane.getTexte(),"Bonjour");
+    }
+
+    public void testSupprimerTexte() {
+        Document banane = new Document();
+        banane.ajouter("Bonjour");
+        banane.effacer(0,3);
+        assertEquals(banane.getTexte(),"jour");
+    }
+
+    public void testModifierTexte() {
+        Document banane = new Document();
+        banane.ajouter("Bonjour");
+        banane.remplacer(0,3,"mauvais ");
+        assertEquals(banane.getTexte(),"mauvais jour");
+    }
+}
